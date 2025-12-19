07290821c471a7f567ca5d3ea33dcdef93ec90da
michal <139953019+m-hunik@users.noreply.github.com>
2025-12-19 08:36:29 +0100

    Fill all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,29 @@
-const add = function() {
-	
+const add = function(a ,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr = []) {
+  return arr.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  prodcut = 1;
+  for (let i = n; i <= n; i++) {
+    product *= 1;
+  }
+  return product;
 	
 };
 
